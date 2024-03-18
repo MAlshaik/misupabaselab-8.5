05687273634b99c5f346d8f68b4b5e3c61f56b09
MAlshaik <alshaik8@msu.edu>
2024-03-17 23:59:23 -0400

    yo

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,5 +1,4 @@
 "use client"
-
 import Image from "next/image";
 import { useEffect, useState } from 'react';
 import {
@@ -14,34 +13,38 @@ import {
 
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://aochmttjnwxqdmbjowmi.supabase.co'
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY
+const supabaseUrl: string = 'https://aochmttjnwxqdmbjowmi.supabase.co'
+const supabaseKey: string = process.env.NEXT_PUBLIC_SUPABASE_KEY || ''
 const supabase = createClient(supabaseUrl, supabaseKey)
 
+interface Book {
+  name: string;
+  author: string;
+  isbn: string;
+}
 
-async function getBooks() {
+async function getBooks(): Promise<Book[]> {
     let { data, error } = await supabase
         .from('books')
         .select('name, author, isbn')
 
     if (error) throw error
     console.log(data)
-    return data
-    }
+    return data || []
+}
 
 export default function Home() {
-    
-  const [names, setNames] = useState(null);
-  const [authors, setAuthors] = useState(null);
-  const [isbn, setIsbn] = useState(null);
+  const [names, setNames] = useState<string[] | null>(null);
+  const [authors, setAuthors] = useState<string[] | null>(null);
+  const [isbn, setIsbn] = useState<string[] | null>(null);
 
   useEffect(() => {
     async function fetchBooks() {
-      const data  = await getBooks();
+      const data: Book[] = await getBooks();
     
-      const names = data.map((book) => book.name);
-      const authors = data.map((book) => book.author);
-      const isbn = data.map((book) => book.isbn);
+      const names: string[] = data.map((book) => book.name);
+      const authors: string[] = data.map((book) => book.author);
+      const isbn: string[] = data.map((book) => book.isbn);
 
       setNames(names);
       setAuthors(authors);
@@ -68,8 +71,8 @@ export default function Home() {
     {names && names.map((name, index) => (
         <TableRow key={index}>
             <TableCell>{name}</TableCell>
-            <TableCell>{authors[index]}</TableCell>
-            <TableCell>{isbn[index]}</TableCell>
+            <TableCell>{authors ? authors[index] : ''}</TableCell>
+            <TableCell>{isbn ? isbn[index] : ''}</TableCell>
         </TableRow>
         ))
     }
@@ -80,3 +83,4 @@ export default function Home() {
    </main>
   );
 }
+
